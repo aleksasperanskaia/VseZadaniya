4baac721f9aecabc4411bda44d98a6ed1ceba6d0
aleksasperanskaia <aleksandrasperanskaya@gmail.com>
2024-12-27 17:40:07 +0400

    calc.cpp

--- 10.12/calc.cpp
+++ 10.12/calc.cpp
@@ -14,10 +14,10 @@ double compute(double a, double b, char op) {
         case '-': return a - b;
         case '*': return a * b;
         case '/':
-            if (b == 0) throw std::runtime_error("Деление на ноль невозможно");
+            if (b == 0) throw std::runtime_error("division by zero is impossible");
             return a / b;
         case '^': return std::pow(a, b);
-        default: throw std::runtime_error("Неизвестный оператор");
+        default: throw std::runtime_error("unknown operation");
     }
 }
 
@@ -25,16 +25,7 @@ double parseTerm(std::istringstream& ss) {
     double result;
     char op;
 
-
-    if (ss.peek() == '(') {
-        ss.get();
-        result = parseExpression(ss);
-        if (ss.get() != ')') {
-            throw std::runtime_error("Скобки не сбалансированы");
-        }
-    }
-
-    else if (isdigit(ss.peek()) || ss.peek() == '.') {
+    if (isdigit(ss.peek()) || ss.peek() == '.') {
         std::string number;
         char c;
         bool pointSeen = false;
@@ -42,7 +33,7 @@ double parseTerm(std::istringstream& ss) {
         while (ss >> c && (isdigit(c) || c == '.')) {
             if (c == '.') {
                 if (pointSeen) {
-                    throw std::runtime_error("Некорректное число: несколько точек");
+                    throw std::runtime_error("dot at the end");
                 }
                 pointSeen = true;
             }
@@ -55,20 +46,16 @@ double parseTerm(std::istringstream& ss) {
         }
 
         result = std::stod(number);
-    }
-
-    else if (ss.peek() == '+' || ss.peek() == '-') {
+    } else if (ss.peek() == '+' || ss.peek() == '-') {
         char sign = ss.get();
         result = parseTerm(ss);
 
         if (sign == '-') {
             result = -result;
         }
+    } else {
+        throw std::runtime_error("Incorrect expression");
     }
-    else {
-        throw std::runtime_error("Некорректное выражение");
-    }
-
 
     while (ss >> std::ws && (ss.peek() == '*' || ss.peek() == '/' || ss.peek() == '^')) {
         ss.get(op);
@@ -83,17 +70,15 @@ double parseExpression(std::istringstream& ss) {
     double result = parseTerm(ss);
     char op;
 
-
     while (ss >> std::ws && (ss.peek() == '+' || ss.peek() == '-')) {
         ss.get(op);
-        double nextTerm = parseTerm(ss);
-
 
         if (op == '-' && ss.peek() == '-') {
             ss.get();
             op = '+';
         }
 
+        double nextTerm = parseTerm(ss);
         result = compute(result, nextTerm, op);
     }
 
@@ -104,13 +89,13 @@ double evaluate(const std::string& expression) {
     std::istringstream ss(expression);
 
     if (expression.empty()) {
-        throw std::runtime_error("Пустой ввод");
+        throw std::runtime_error("empty input");
     }
 
     double result = parseExpression(ss);
 
     if (ss >> std::ws && !ss.eof()) {
-        throw std::runtime_error("Некорректное выражение");
+        throw std::runtime_error("Incorrect expression");
     }
 
     return result;
